5ea0f434cdf0786d943916bf298118c37554187c
KanishkKalyani <you@example.comkanishkkalyani@gmail.com>
2020-06-10 00:14:59 +0530

    Updates

---
+++ precache-manifest.ceb14532939f035902e79da9afcb194c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ab657250289ef5da4d72bb11ab555fb",
+    "url": "/React-Calculator/index.html"
+  },
+  {
+    "revision": "9224a5119e50aa3cb550",
+    "url": "/React-Calculator/static/css/main.80a5a2b9.chunk.css"
+  },
+  {
+    "revision": "d194041cf6f17c3a8cfc",
+    "url": "/React-Calculator/static/js/2.eb570e80.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Calculator/static/js/2.eb570e80.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9224a5119e50aa3cb550",
+    "url": "/React-Calculator/static/js/main.86a33923.chunk.js"
+  },
+  {
+    "revision": "be48d395d4484ed76dfb",
+    "url": "/React-Calculator/static/js/runtime-main.65f13dfc.js"
+  }
+]);
